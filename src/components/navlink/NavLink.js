a77d4c109c5cbaad7ea6a73067828e4927be6e7c
dipesh-dev99 <dipesh.bhatta@yutitech.in>
2025-05-30 05:32:21 -0600

    landing, navbar

--- src/components/navlink/NavLink.js
+++ src/components/navlink/NavLink.js
@@ -2,12 +2,12 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export default function NavLink({ href, children }) {
+export default function NavLink({ href, children, className = '', ...props }) {
   const pathname = usePathname();
   const isActive = pathname === href;
 
   return (
-    <Link href={href} className={`${isActive ? "font-semibold" : ""}`}>
+    <Link href={href} className={`${isActive ? "font-semibold" : ""} ${className}`} {...props}>
       {children}
     </Link>
   );
