6fb26460dec51e6f63bc0b91b29d9953f8182184
Ananya <ananya@emproto.com>
2025-06-12 18:09:08 +0530

    faq image change and aligent

--- src/components/FAQ/FaqSection.js
+++ src/components/FAQ/FaqSection.js
@@ -3,41 +3,50 @@ const FaqSection = () => {
     <section className="relative w-full">
       <div className="faq-hero">
         <div className="faq-header">
-          <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold">Frequently Asked Questions</h1>
-          <p className="text-base sm:text-lg md:text-xl">Answers to the questions your patients ask the most</p>
+          <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold">
+            Frequently Asked Questions
+          </h1>
+          <p className="text-base sm:text-lg md:text-xl">
+            Answers to the questions your patients ask the most
+          </p>
           <div className="relative w-full">
-            <input 
-              type="text" 
-              placeholder="Search" 
+            <input
+              type="text"
+              placeholder="Search"
               className="faq-search w-full max-w-lg mx-auto"
               aria-label="Search FAQs"
             />
             <div className="faq-search-icon">
-            <svg
-              
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="#006D38"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              aria-hidden="true"
-            >
-              <circle cx="11" cy="11" r="8"></circle>
-              <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-            </svg>
-              
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#006D38"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                aria-hidden="true"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+              </svg>
             </div>
           </div>
         </div>
       </div>
 
       <div className="w-full flex justify-center px-2 sm:px-4 py-8 sm:py-16">
-        <div className="space-y-4 w-full max-w-2xl">
-          <details className="card card-p-0 card2 border-light group cursor-pointer" style={{ borderRadius: '20px', borderWidth: '1px', padding: '30px' }}>
+        <div className="space-y-4 w-full max-w-5xl">
+          <details
+            className="card card-p-0 card2 border-[#004C44] group cursor-pointer"
+            style={{
+              borderRadius: "20px",
+              borderWidth: "1px",
+              padding: "30px",
+            }}
+          >
             <summary className="flex justify-between items-center">
               <span>Is Aligner treatment painful?</span>
               <svg
@@ -56,13 +65,27 @@ const FaqSection = () => {
               </svg>
             </summary>
             <div>
-              <p>Some discomfort or tooth soreness is common, especially when starting a new set of aligners. This is a normal sign that your teeth are shifting as planned. The discomfort usually fades within a few days. If it persists, contact your doctor.</p>
+              <p>
+                Some discomfort or tooth soreness is common, especially when
+                starting a new set of aligners. This is a normal sign that your
+                teeth are shifting as planned. The discomfort usually fades
+                within a few days. If it persists, contact your doctor.
+              </p>
             </div>
           </details>
 
-          <details className="card card-p-0 card2 border-light group cursor-pointer" style={{ borderRadius: '20px', borderWidth: '1px', padding: '30px' }}>
+          <details
+            className="card card-p-0 card2 border-[#004C44] group cursor-pointer"
+            style={{
+              borderRadius: "20px",
+              borderWidth: "1px",
+              padding: "30px",
+            }}
+          >
             <summary className="flex justify-between items-center">
-              <span className="font-semibold text-lg">Are there food restrictions during aligner treatment?</span>
+              <span className="font-semibold text-lg">
+                Are there food restrictions during aligner treatment?
+              </span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -79,13 +102,26 @@ const FaqSection = () => {
               </svg>
             </summary>
             <div>
-              <p>No food restrictions! Just remove your aligners before eating or drinking anything except water. Remember to brush your teeth before putting them back in.</p>
+              <p>
+                No food restrictions! Just remove your aligners before eating or
+                drinking anything except water. Remember to brush your teeth
+                before putting them back in.
+              </p>
             </div>
           </details>
 
-          <details className="card card-p-0 card2 border-light group cursor-pointer" style={{ borderRadius: '20px', borderWidth: '1px', padding: '30px' }}>
+          <details
+            className="card card-p-0 card2 border-[#004C44] group cursor-pointer"
+            style={{
+              borderRadius: "20px",
+              borderWidth: "1px",
+              padding: "30px",
+            }}
+          >
             <summary className="flex justify-between items-center">
-              <span className="font-semibold text-lg">Can I drink hot or cold beverages while wearing my aligners?</span>
+              <span className="font-semibold text-lg">
+                Can I drink hot or cold beverages while wearing my aligners?
+              </span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -102,13 +138,26 @@ const FaqSection = () => {
               </svg>
             </summary>
             <div>
-              <p>It&apos;s recommended to only drink water while wearing your aligners. Hot beverages can warp the plastic, and other drinks can stain them or get trapped between your teeth and aligners.</p>
+              <p>
+                It&apos;s recommended to only drink water while wearing your
+                aligners. Hot beverages can warp the plastic, and other drinks
+                can stain them or get trapped between your teeth and aligners.
+              </p>
             </div>
           </details>
 
-          <details className="card card-p-0 card2 border-light group cursor-pointer" style={{ borderRadius: '20px', borderWidth: '1px', padding: '30px' }}>
+          <details
+            className="card card-p-0 card2 border-[#004C44] group cursor-pointer"
+            style={{
+              borderRadius: "20px",
+              borderWidth: "1px",
+              padding: "30px",
+            }}
+          >
             <summary className="flex justify-between items-center">
-              <span className="font-semibold text-lg">Can I chew gum while wearing aligners?</span>
+              <span className="font-semibold text-lg">
+                Can I chew gum while wearing aligners?
+              </span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -125,13 +174,26 @@ const FaqSection = () => {
               </svg>
             </summary>
             <div>
-              <p>No, you should not chew gum while wearing aligners. Gum can stick to them and damage the plastic. Remove your aligners if you want to chew gum.</p>
+              <p>
+                No, you should not chew gum while wearing aligners. Gum can
+                stick to them and damage the plastic. Remove your aligners if
+                you want to chew gum.
+              </p>
             </div>
           </details>
 
-          <details className="card card-p-0  card2 border-light group cursor-pointer" style={{ borderRadius: '20px', borderWidth: '1px', padding: '30px' }}>
+          <details
+            className="card card-p-0  card2 border-[#004C44] group cursor-pointer"
+            style={{
+              borderRadius: "20px",
+              borderWidth: "1px",
+              padding: "30px",
+            }}
+          >
             <summary className="flex justify-between items-center">
-              <span className="font-semibold text-lg">Will smoking or chewing tobacco stain my aligners?</span>
+              <span className="font-semibold text-lg">
+                Will smoking or chewing tobacco stain my aligners?
+              </span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -148,13 +210,26 @@ const FaqSection = () => {
               </svg>
             </summary>
             <div>
-              <p>Yes, smoking or using tobacco products can stain your aligners. We recommend removing aligners when smoking and cleaning them thoroughly before putting them back in.</p>
+              <p>
+                Yes, smoking or using tobacco products can stain your aligners.
+                We recommend removing aligners when smoking and cleaning them
+                thoroughly before putting them back in.
+              </p>
             </div>
           </details>
 
-          <details className="card card-p-0 card-p-0 card2 border-light group cursor-pointer" style={{ borderRadius: '20px', borderWidth: '1px', padding: '30px' }}>
+          <details
+            className="card card-p-0 card-p-0 card2 border-[#004C44] group cursor-pointer"
+            style={{
+              borderRadius: "20px",
+              borderWidth: "1px",
+              padding: "30px",
+            }}
+          >
             <summary className="flex justify-between items-center">
-              <span className="font-semibold text-lg">How much should I soak direct aligner in hot water?</span>
+              <span className="font-semibold text-lg">
+                How much should I soak direct aligner in hot water?
+              </span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -171,13 +246,26 @@ const FaqSection = () => {
               </svg>
             </summary>
             <div>
-              <p>Follow your doctor&apos;s specific instructions for soaking aligners. Generally, use lukewarm (not hot) water and approved cleaning solutions only.</p>
+              <p>
+                Follow your doctor&apos;s specific instructions for soaking
+                aligners. Generally, use lukewarm (not hot) water and approved
+                cleaning solutions only.
+              </p>
             </div>
           </details>
 
-          <details className="card card-p-0 card2 border-light group cursor-pointer" style={{ borderRadius: '20px', borderWidth: '1px', padding: '30px' }}>
+          <details
+            className="card card-p-0 card2 border-[#004C44] group cursor-pointer"
+            style={{
+              borderRadius: "20px",
+              borderWidth: "1px",
+              padding: "30px",
+            }}
+          >
             <summary className="flex justify-between items-center">
-              <span className="font-semibold text-lg">What should I do if I lose or break an aligner?</span>
+              <span className="font-semibold text-lg">
+                What should I do if I lose or break an aligner?
+              </span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -194,13 +282,26 @@ const FaqSection = () => {
               </svg>
             </summary>
             <div>
-              <p>Contact your doctor immediately if you lose or break an aligner. They will advise whether to move to the next set or order a replacement.</p>
+              <p>
+                Contact your doctor immediately if you lose or break an aligner.
+                They will advise whether to move to the next set or order a
+                replacement.
+              </p>
             </div>
           </details>
 
-          <details className="card card-p-0 card2 border-light group cursor-pointer" style={{ borderRadius: '20px', borderWidth: '1px', padding: '30px' }}>
+          <details
+            className="card card-p-0 card2 border-[#004C44] group cursor-pointer"
+            style={{
+              borderRadius: "20px",
+              borderWidth: "1px",
+              padding: "30px",
+            }}
+          >
             <summary className="flex justify-between items-center">
-              <span className="font-semibold text-lg">What happens if I lose or break an attachment?</span>
+              <span className="font-semibold text-lg">
+                What happens if I lose or break an attachment?
+              </span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -217,7 +318,10 @@ const FaqSection = () => {
               </svg>
             </summary>
             <div>
-              <p>If an attachment falls off or breaks, schedule an appointment with your doctor as soon as possible to have it replaced.</p>
+              <p>
+                If an attachment falls off or breaks, schedule an appointment
+                with your doctor as soon as possible to have it replaced.
+              </p>
             </div>
           </details>
         </div>
