3d1470fb0b4b3f225af03f2013096ee105a58189
Ananya <ananya@emproto.com>
2025-06-12 14:21:48 +0530

    Pre Login : Home page Service Offered Circle chnaged and hero alignment

--- src/components/home/hero-section.js
+++ src/components/home/hero-section.js
@@ -5,13 +5,27 @@ export default function HeroSection() {
   return (
     <section className={`${styles.heroSection} `}>
       {/* Content */}
-      <div className={`container mx-auto ${styles.heroContent}`}>
-        <div className="max-w-3xl w-full">
-          <h1 className="text-2xl sm:text-4xl md:text-5xl lg:text-5xl font-[600] mb-6 leading-tight text-[#184C3A]">
-            Designed for the<br />Smiles You Shape
+      <div className={`container ${styles.heroContent}`}>
+        <div className="max-w-5xl w-full">
+          <h1 className="text-2xl sm:text-[2.25rem] md:text-[2.875rem] lg:text-[2.875rem] font-semibold mb-6 leading-tight text-[#004C44]">
+            Designed for the
+            <br />
+            Smiles You Shape
           </h1>
-          <p className="text-base md:text-xl lg:text-[1.3rem] font-[500] text-justify text-[#184C3A]">
-            At Synapse, biomechanical insights and personalized workflows come together to support your clinical precision and elevate every treatment journey. From meticulous treatment planning to diligent manufacturing, each solution is crafted for comfort, control, and optimal outcomes. Backed by the latest in 3D technology, our partnership is built around your expertise—and every smile you help create.
+          <p className="text-base md:text-[1.25rem] lg:text-[1.25rem] font-medium text-justify text-[#004C44]">
+            At Synapse, biomechanical insights and personalized workflows
+            <br />
+            come together to support your clinical precision and elevate
+            <br />
+            every treatment journey.
+            <br />
+            From meticulous treatment planning to diligent manufacturing,
+            <br />
+            each solution is crafted for comfort, control, and optimal
+            <br />
+            outcomes. Backed by the latest in 3D technology, our partnership
+            <br />
+            is built around your expertise—and every smile you help create.
           </p>
         </div>
       </div>
