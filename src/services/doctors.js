5fba4449a76fe669abcc85c69d3565f617d07c8a
Ananya <ananya@emproto.com>
2025-06-14 19:40:00 +0530

    notification on admin approve or reject

--- src/services/doctors.js
+++ src/services/doctors.js
@@ -1,39 +1,52 @@
 import instance from "@/network/index";
+import toast from "react-hot-toast";
+
 
 
 export const fetchDoctors = async (queryString) => {
-    try {
-        const res = await instance.get(`/admin/get_all_users${queryString}`); // Adjust path if needed
-        return res.data
-            ; // Axios wraps the data inside `data`
-    } catch (error) {
-        const message =
-            error?.response?.data?.message || error?.message || "Failed to fetch doctors";
-        throw new Error(message);
-    }
+  try {
+    const res = await instance.get(`/admin/get_all_users${queryString}`);
+
+    toast.success("Doctors list fetched successfully");
+    return res.data;
+  } catch (error) {
+    const message =
+      error?.response?.data?.message ||
+      error?.message ||
+      "Failed to fetch doctors";
+
+    toast.error(message);
+
+    throw new Error(message);
+  }
 };
 
 export const approveDoctors = async (userId) => {
-    try {
-        const res = await instance.get(`/admin/approve_user?user_id=${userId}`); // Adjust path if needed
-        return res.data
-            ; // Axios wraps the data inside `data`
-    } catch (error) {
-        const message =
-            error?.response?.data?.message || error?.message || "Failed to approve doctors";
-        throw new Error(message);
-    }
+  try {
+    const res = await instance.get(`/admin/approve_user?user_id=${userId}`);
+    toast.success("Doctor Approved");
+    return res.data;
+  } catch (error) {
+    const message =
+      error?.response?.data?.message ||
+      error?.message ||
+      "Failed to approve doctor";
+    toast.error(message);
+    throw new Error(message);
+  }
 };
 
-
 export const rejectDoctors = async (userId) => {
-    try {
-        const res = await instance.get(`/admin/reject_user?user_id=${userId}`); // Adjust path if needed
-        return res.data
-            ; // Axios wraps the data inside `data`
-    } catch (error) {
-        const message =
-            error?.response?.data?.message || error?.message || "Failed to reject doctors";
-        throw new Error(message);
-    }
+  try {
+    const res = await instance.get(`/admin/reject_user?user_id=${userId}`);
+    toast.success("Doctor rejected");
+    return res.data;
+  } catch (error) {
+    const message =
+      error?.response?.data?.message ||
+      error?.message ||
+      "Failed to reject doctor";
+    toast.error(message);
+    throw new Error(message);
+  }
 };
