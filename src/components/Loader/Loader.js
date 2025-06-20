6029b0d7d9a63910686b54826708e74a8983a8fd
dev-ananya <ananya.singh@yutitech.in>
2025-06-20 15:29:18 +0530

    Loader component

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+const Loader = () => (
+  <div className={styles.overlay}>
+    <div className={styles.loader}></div>
+  </div>
+);
+
+export default Loader;
